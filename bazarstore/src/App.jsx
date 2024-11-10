fbfa90fdd0f321cd34ffaf75f24ec08e71368d26
aybeniz-24 <elesgerovaaybeniz526@gmail.com>
2024-11-11 01:59:00 +0400

    first commit

---
+++ bazarstore/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import Header from "./components/header/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes> */}
+
+
+
+    
+    </>
+
+  )
+}
+
+export default App
---
+++ bazarstore/src/app.js
@@ -0,0 +1,25 @@
+// updateTitleAndFavicon.js
+
+export const updateTitleAndFavicon = () => {
+  const titles = [
+    "Salam, bir dəqiqə....",
+    "Biz hələ də burdayıq."
+  ];
+  const favicons = [
+    "/heart.ico",
+    "/heart.ico"
+  ];
+
+  let titleIndex = 0
+  let faviconIndex = 0
+
+  setInterval(() => {
+    document.title = titles[titleIndex]
+
+    const link = document.querySelector('link[rel="shortcut icon"]')
+    link.href = favicons[faviconIndex]
+
+    titleIndex = (titleIndex + 1) % titles.length
+    faviconIndex = (faviconIndex + 1) % favicons.length
+  }, 1500)
+}
